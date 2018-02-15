08518d29b1481b74620d3e1eba48c9c3b5ae2181
Josh Satriano <jdsatch@gmail.com>
2018-02-15 00:38:49 -0600

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort])
+    @sort = params[:var]
   end
 
   def new
