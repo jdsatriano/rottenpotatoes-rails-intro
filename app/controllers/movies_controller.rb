0cba898121841029a8220ed5f9a57607d523f1f7
Josh Satriano <jdsatch@gmail.com>
2018-02-18 21:53:34 -0600

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,28 +14,66 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.uniq.pluck("rating")
 
     #if 'refresh' button pressed, get checked ratings
-    #else get all ratings
+    #else check if session[:checked] is set, else just use all ratings
     if params[:ratings]
       @checked = params[:ratings].keys
+      session[:checked] = @checked
+    elsif session[:checked]
+      @checked = session[:checked]
     else
       @checked = @all_ratings
-    end
-
-    #if getting movies by sorting movie_title or release_date
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
-      @sort = params[:sort]
-    #else if get the movies by rating rilter
-    elsif params[:ratings]
-      @movies = Movie.where(:rating => @checked)
-    #else just get all movies
-    else
-      @movies = Movie.all
+      session[:checked] = @checked
     end
 
     @checked.each do |rating|
       params[rating] = true
     end
+
+    #if getting movies by sorting movie_title or release_date
+    #check if session[:checked] is set
+    if params[:sort]
+      if session[:checked]
+        @movies = Movie.where(:rating => session[:checked]).order(params[:sort])
+      else
+        @movies = Movie.order(params[:sort])
+      end
+
+      @sort = params[:sort]
+      session[:sort] = @sort
+
+    #else if get the movies by rating rilter
+    #check if session[:sort] is set
+    elsif params[:ratings]
+      if session[:sort]
+        @movies = Movie.where(:rating => @checked).order(session[:sort])
+        params[:sort] = session[:sort]
+      else
+        @movies = Movie.where(:rating => @checked)
+      end
+
+      @sort = session[:sort]
+
+    #if session has been started
+    #render page accordingly
+    elsif session[:started]
+      if session[:checked] and session[:sort]
+        @movies = Movie.where(:rating => session[:checked]).order(session[:sort])
+        params[:sort] = session[:sort]
+      elsif session[:checked]
+        @movies = Movie.where(:rating => session[:checked])
+      elsif session[:sort]
+        @movies = Movie.order(session[:sort])
+        params[:sort] = session[:sort]
+      else
+        @movies = Movie.all
+      end
+
+    else
+      #start session
+      session[:started] = true
+      @movies = Movie.all
+    end
+
   end
 
   def new
